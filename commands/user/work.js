69b37fca898e4f74c820e575b308f891df91f5b7
Matthew Flammia <matthewfrankflammia@gmail.com>
2023-05-06 19:52:37 -0400

    another work debug

--- commands/user/work.js
+++ commands/user/work.js
@@ -38,7 +38,7 @@ module.exports = {
 			//roll the dice
 			let best = 0;
 			for(let i=0;i<=user_stats.luck;i++){
-				let coinReward = Math.floor(Math.random()*(upper_bound-lower_bound+1))+lower_bound;
+				let coinReward = Math.floor(Math.random()*(upper_bound-lower_bound+1)+lower_bound);
 				if(coinReward > best)
 					best = coinReward;
 			}
